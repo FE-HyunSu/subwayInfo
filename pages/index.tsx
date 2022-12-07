bf2b8790f344fba391e02503ae881dc65a74822e
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-07 09:08:17 +0900

    add, modify : api get.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,47 @@ import Sample from "../components/sample/body";
 import axios from "axios";
 
 const Index = () => {
-  const [test, setTest] = useState<boolean>(false);
+  const [dataList, setDataList] = useState<any>([]);
+  const apiGet = async () => {
+    try {
+      let queryParams =
+        "?" +
+        encodeURIComponent("serviceKey") +
+        "=" +
+        process.env.NEXT_PUBLIC_SERVICE_KEY;
+      queryParams +=
+        "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
+      queryParams +=
+        "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10");
+      queryParams +=
+        "&" + encodeURIComponent("dataType") + "=" + encodeURIComponent("JSON");
+      queryParams +=
+        "&" +
+        encodeURIComponent("CURRENT_DATE") +
+        "=" +
+        encodeURIComponent("2019122010");
+      queryParams +=
+        "&" + encodeURIComponent("HOUR") + "=" + encodeURIComponent("24");
+      queryParams +=
+        "&" + encodeURIComponent("COURSE_ID") + "=" + encodeURIComponent("1");
+      console.log(queryParams);
+      const response = await axios.get(
+        `http://apis.data.go.kr/1360000/TourStnInfoService/getTourStnVilageFcst${queryParams}`
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.error(e);
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_SERVICE_KEY);
+    apiGet();
+  }, []);
+
   return (
     <>
-      <p>{test && test ? `true` : `false`}</p>
       <Sample />
     </>
   );
