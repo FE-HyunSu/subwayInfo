d27ab046d057bafb9c8852fd3bb1445a3d25b9b5
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-09 22:30:19 +0900

    modify : api data export.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,21 +6,33 @@ import axios from "axios";
 import dayjs from "dayjs";
 const Index = () => {
   const [recoilItem, setRecoilItem] = useRecoilState(dataSet);
+  const [testArr, setTestArr] = useState<any>();
   const serviceKey = process.env.NEXT_PUBLIC_DATA_GO_KR_KEY;
   const apiGet = async () => {
     try {
       const response = await axios.get(
-        `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${serviceKey}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20221207&base_time=0600&nx=55&ny=127`
+        `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${serviceKey}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20221209&base_time=0600&nx=55&ny=127`
       );
-      console.log(response.data);
+      await setTestArr(response.data.response.body.items.item);
+      await console.log(response.data.response.body.items.item);
     } catch (e) {
       console.error(e);
     }
   };
 
+  useEffect(() => {
+    apiGet();
+  }, []);
+
   return (
     <>
       <Sample />
+      {console.log(testArr?.length)}
+      {testArr &&
+        testArr.length > 0 &&
+        testArr.map((item: any, idx: any) => (
+          <div key={idx}>{item.baseDate}</div>
+        ))}
     </>
   );
 };
