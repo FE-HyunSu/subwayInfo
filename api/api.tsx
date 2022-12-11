06f412249fa5648af155a17b874ce92193e03602
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-11 19:41:20 +0900

    feature : api https -> http 원복.

--- api/api.tsx
+++ api/api.tsx
@@ -5,7 +5,7 @@ const dataServiceKey = process.env.NEXT_PUBLIC_DATA_GO_KR_KEY;
 export const getSubwayInfo = async (name: string) => {
   console.log(name);
   return await axios.get(
-    `https://swopenAPI.seoul.go.kr/api/subway/${swServiceKey}/json/realtimeStationArrival/1/100/${encodeURIComponent(
+    `http://swopenAPI.seoul.go.kr/api/subway/${swServiceKey}/json/realtimeStationArrival/1/100/${encodeURIComponent(
       name
     )}`
   );
@@ -13,6 +13,6 @@ export const getSubwayInfo = async (name: string) => {
 
 export const getWeatherInfo = async () => {
   return await axios.get(
-    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${dataServiceKey}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20221209&base_time=0600&nx=55&ny=127`
+    `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${dataServiceKey}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20221209&base_time=0600&nx=55&ny=127`
   );
 };
