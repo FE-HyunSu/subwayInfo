6423a78938caa03f968805cd68ba0c71e8d42e60
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-13 23:45:58 +0900

    feature : isLoading.

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -21,6 +21,7 @@ const Detail = () => {
   const router = useRouter();
   const [isHeaderMode, setHeaderMode] = useRecoilState(headerMode);
   const [responseInfo, setResponseInfo] = useState<subwayInfoType>();
+  const [isLoading, setLoading] = useState<boolean>(true);
   const [stationName, setStationName] = useState<string | undefined>(
     decodeURIComponent(router.asPath?.split(`station=`)[1]?.split("&")[0])
   );
@@ -30,8 +31,12 @@ const Detail = () => {
       console.log(response);
       await console.log(response.data.realtimeArrivalList);
       await setResponseInfo(response.data.realtimeArrivalList);
-    } catch (e) {
+    } catch (e: any) {
       console.log(`error : `, e);
+      if (e && e.code === "ERR_NETWORK") {
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -42,18 +47,22 @@ const Detail = () => {
 
   return (
     <>
-      {responseInfo &&
-        responseInfo.map((item: subwayInfoType, idx: number) => (
-          <div key={idx}>
-            <TrainInfo
-              line={2}
-              subWayName={item.statnNm}
-              msg1={item.arvlMsg2}
-              msg2={item.arvlMsg3}
-              updnLine={item.updnLine}
-              btrainNo={item.btrainNo}
-            />
-            {/* 대상 지하철 역 : 잠실
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {responseInfo &&
+            responseInfo.map((item: subwayInfoType, idx: number) => (
+              <div key={idx}>
+                <TrainInfo
+                  line={2}
+                  subWayName={item.statnNm}
+                  msg1={item.arvlMsg2}
+                  msg2={item.arvlMsg3}
+                  updnLine={item.updnLine}
+                  btrainNo={item.btrainNo}
+                />
+                {/* 대상 지하철 역 : 잠실
 열차도착정보를 생성한 시각: 2022-12-13 22:59:28
 첫번째도착메세지 (전역 진입, 전역 도착 등): 전역 출발
 두번째도착메세지 (종합운동장 도착, 12분 후 (광명사거리) 등): 석촌
@@ -61,12 +70,16 @@ const Detail = () => {
 열차번호: 8276
 상하행선구분(2호선 : (내선:0,외선:1),상행,하행): 상행
 열차종류(급행,ITX) : */}
-            <p>도착지방면(성수행 - 구로디지털단지방면): {item.trainLineNm}</p>
-            <p>열차번호: {item.btrainNo}</p>
-            <p>열차종류(급행,ITX) : {item.btrainSttus}</p>
-            <p>&nbsp;</p>
-          </div>
-        ))}
+                <p>
+                  도착지방면(성수행 - 구로디지털단지방면): {item.trainLineNm}
+                </p>
+                <p>열차번호: {item.btrainNo}</p>
+                <p>열차종류(급행,ITX) : {item.btrainSttus}</p>
+                <p>&nbsp;</p>
+              </div>
+            ))}
+        </>
+      )}
     </>
   );
 };
